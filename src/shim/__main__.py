b63e4904c7718b579e75bbbae2225a9aec92e6c5
Marius Guščius <marius.guscius@mailbox.org>
2025-08-10 15:26:47 +0300

    components

--- src/shim/__main__.py
+++ src/shim/__main__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from questionary import autocomplete
 from rich.console import Console
 from rich.table import Table
 from rich.panel import Panel
@@ -7,7 +8,14 @@ from typing import Any
 from yaml import safe_load
 from .runner import run
 
+recipes_dir: Path = Path("recipes")
+recipes: list[Path] = sorted({
+    flow_file.parent.relative_to(recipes_dir)
+    for flow_file in recipes_dir.rglob("flow.json")
+})
+
 def deploy() -> None:
+    autocomplete("Please select a recipe", choices = []).ask()
     for line in run("--help"):
         print(line)
 
@@ -16,7 +24,6 @@ def destroy() -> None:
         print(line)
 
 def list() -> None:
-    base_path: Path = Path("recipes")
     console: Console = Console()
     table: Table = Table(
         title = "🍳 Available Recipes",
@@ -27,22 +34,22 @@ def list() -> None:
     table.add_column("Recipe", style = "bold yellow", no_wrap = True)
     table.add_column("Version")
     table.add_column("Description")
-    table.add_column("URL")
-    dependency_file: Path
-    for dependency_file in sorted(base_path.rglob("dependency.json")):
-        recipe_dir: Path = dependency_file.parent
-        recipe: str = "/".join(recipe_dir.relative_to(base_path).parts)
+    table.add_column("Components")
+    for recipe in recipes:
         meta_version: str = "(no version provided)"
         meta_description: str = "(no description provided)"
-        meta_url: str = "(no URL provided)"
-        if ((meta_file := recipe_dir / "meta.yml").exists()):
+        meta_components: str = "(no components provided)"
+        if ((meta_file := recipes_dir / recipe / "meta.yml").exists()):
             meta: dict[str, Any] = safe_load(meta_file.read_bytes())
-            if meta["version"]:
-                meta_version = meta["version"]
-            if meta["description"]:
-                meta_description = meta["description"]
-            if meta["url"]:
-                meta_url = f"[link={meta['url']}]{meta['url']}[/link]"
-        table.add_row(recipe, meta_version, meta_description, meta_url)
+            if meta['version']:
+                meta_version = meta['version']
+            if meta['description']:
+                meta_description = meta['description']
+            if meta['components']:
+                meta_components = "\n".join([
+                    f"[link={component}]{component}[/link]"
+                    for component in meta['components']
+                ])
+        table.add_row(str(recipe), meta_version, meta_description, meta_components)
     panel: Panel = Panel.fit(table, border_style = "bright_blue")
     console.print(panel)
