3553e98a1ce1cd1990eee10ca7f51c1a39412579
Marius Guščius <marius.guscius@mailbox.org>
2026-07-18 11:31:43 +0300

    symlinks instead of file copies

--- scripts/link_shared.sh
+++ scripts/link_shared.sh
@@ -4,6 +4,7 @@ SHARED_HELPERS=($(find shared/ -mindepth 1))
 
 for PACK in $(find packs/ -mindepth 1 -maxdepth 1 -type d); do
     for HELPER in "${SHARED_HELPERS[@]}"; do
-        cp -f ${HELPER} ${PACK}/templates/
+        HELPER_NAME="$(basename "${HELPER}")"
+        ln -sf "../../../shared/${HELPER_NAME}" "${PACK}/templates/${HELPER_NAME}"
     done
 done
